2c1f09fedb0dbfe8bdbd9314c68ff62ddc2cdd12
Kilian <glase@uni-potsdam.de>
2022-11-03 21:27:04 +0100

    finished issue 16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,5 @@ class Paper < ApplicationRecord
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true}
   has_and_belongs_to_many :authors
+  scope :year, ->(year) { where("year = ?", year) if year.present?}
 end
