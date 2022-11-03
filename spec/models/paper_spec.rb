63a986065492833a2fbca96bc8a8052dbfae677b
Kilian <glase@uni-potsdam.de>
2022-11-03 20:15:42 +0100

    finished issue 11

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe Paper, type: :model do
     expect(paper.year).to eq(2000)
     expect(paper).to be_valid
   end
+
+  it "should not be valid without a title" do
+    paper = Paper.new(title: "", venue: "venue", year: 2000)
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid without a venue" do
+    paper = Paper.new(title: "title", venue: "", year: 2000)
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid without a year" do
+    paper = Paper.new(title: "title", venue: "venue")
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid with a non numerical year" do
+    paper = Paper.new(title: "title", venue: "venue", year: "year")
+    expect(paper).to_not be_valid
+  end
 end
